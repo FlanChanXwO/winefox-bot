11fde9cbd0134b29adf2d3b21624904fc16c8fd5
FlanChanOwO <badapple495@outlook.com>
2026-01-11 02:29:02 +0800

    移除未使用的StructuredTaskScope导入

--- src/main/java/com/github/winefoxbot/plugins/dailyreport/DailyReportPlugin.java
+++ src/main/java/com/github/winefoxbot/plugins/dailyreport/DailyReportPlugin.java
@@ -21,7 +21,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.concurrent.StructuredTaskScope;
 
 /**
  * @author FlanChan
